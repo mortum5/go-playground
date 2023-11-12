d5c7d276f870c766e205c5e56678e9548b8fd984
Andrei Gonchar <jeron312@gmail.com>
2023-11-12 21:04:31 +0300

    feat: add simple rabbitmq sender and receiver :sparkles::sparkles::sparkles:

---
+++ rabbitmq/go.mod
@@ -0,0 +1,5 @@
+module github.com/mortum5/rabbitmq
+
+go 1.21.0
+
+require github.com/rabbitmq/amqp091-go v1.9.0
---
+++ rabbitmq/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func send(ctx context.Context, ch *amqp.Channel, q amqp.Queue) {
+	body := "Hello World!"
+	err := ch.PublishWithContext(ctx,
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s\n", body)
+}
+
+func receive(ctx context.Context, ch *amqp.Channel, q amqp.Queue) {
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	go send(ctx, ch, q)
+	go receive(ctx, ch, q)
+
+	select {}
+
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
